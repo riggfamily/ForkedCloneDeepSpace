d10d1b03af2d6f502d63e92878880bd129e6d3fb
Sharon Rigg <riggfamily@cox.net>
2018-10-08 19:32:55 -0700

    Update SampleRobot/src/org/usfirst/frc/team6995/Robot.java

--- SampleRobot/src/org/usfirst/frc/team6995/Robot.java
+++ SampleRobot/src/org/usfirst/frc/team6995/Robot.java
@@ -21,6 +21,7 @@ import org.usfirst.frc.team6995.commands.*;
 import org.usfirst.frc.team6995.subsystems.*;
 
 /**
+ * Ben's Test Code
  * The VM is configured to automatically run this class, and to call the
  * functions corresponding to each mode, as described in the TimedRobot
  * documentation. If you change the name of this class or the package after
